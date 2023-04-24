90f9d96bf4f123c36b0fd7d68ec664db62842f6a
arvindk07 <info.arvind07@gmail.com>
2023-04-24 07:07:55 +0530

    auto import and increament/deceament

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div>
+      <button @click="increament">Increament</button>
+      <h1>{{ count }}</h1>
+      <button @click="decreament">Decreament</button>
+    </div>
   </div>
 </template>
+
+<script setup>
+const count = ref(0);
+
+function increament() {
+  count.value++;
+}
+
+function decreament() {
+  count.value--;
+}
+</script>
