2c87d8a149af17debf8e159c9d9c8a7a0f540263
arvindk07 <info.arvind07@gmail.com>
2023-04-24 17:09:45 +0530

    coditional component method 3

--- app.vue
+++ app.vue
@@ -1,20 +1,16 @@
 <template>
-  <div>
-    <!-- <HomeWelcome /> -->
-    <button @click="toggle">Toggle</button>
-    <!-- <component :is="MyComponent" /> -->
-    <div v-if="MyComponent === 'welcome'">
-      <HomeWelcome />
-    </div>
-    <Counter v-else />
-  </div>
+  <a href="#" @click="view('home')">Home</a>
+  <a href="#" @click="view('counter')">Counter</a>
+
+  <HomeWelcome v-if="page === 'home'" />
+  <Counter v-if="page === 'counter'" />
 </template>
 
 <script setup>
 // const MyComponent = ref(resolveComponent("Counter"));
-const MyComponent = ref("counter");
+const page = ref("home");
 
-function toggle() {
-  MyComponent.value = "welcome";
+function view(name) {
+  page.value = name;
 }
 </script>
