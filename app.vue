0229dda14a27f2d7c38f8d062f9b10a641ba7c07
arvindk07 <info.arvind07@gmail.com>
2023-04-24 07:24:28 +0530

    components and how to directory components import

--- app.vue
+++ app.vue
@@ -1,37 +1,6 @@
 <template>
   <div>
-    <div>
-      <button @click="increament">Increament</button>
-      <h1>{{ count }}</h1>
-      <button @click="decreament">Decreament</button>
-    </div>
+    <HomeWelcome />
+    <Counter />
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      count: 0,
-    };
-  },
-  methods: {
-    increament() {
-      this.count++;
-    },
-    decreament() {
-      this.count++;
-    },
-  },
-};
-
-// const count = ref(0);
-
-// function increament() {
-//   count.value++;
-// }
-
-// function decreament() {
-//   count.value--;
-// }
-</script>
---
+++ components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="increament">Increament</button>
+    <h1>{{ count }}</h1>
+    <button @click="decreament">Decreament</button>
+  </div>
+</template>
+
+<script setup>
+const count = ref(0);
+
+function increament() {
+  count.value++;
+}
+
+function decreament() {
+  count.value--;
+}
+</script>
