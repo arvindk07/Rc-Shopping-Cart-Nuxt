5d1cd939f3cc9dd731dcf1b33c43600532362e9b
arvindk07 <info.arvind07@gmail.com>
2023-04-26 16:45:47 +0530

    layout and custom layout

---
+++ Pages/profile.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>welcom arvind profile</p>
+</template>
+
+<script>
+definePageMeta({
+  layout: "sidebar",
+});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <nav>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/profile">Profile</NuxtLink>
   </nav>
   <slot></slot>
 </template>
